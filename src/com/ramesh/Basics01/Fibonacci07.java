45e3f38ce59f3ff23abe76af0a38a5d6fae4cc59
ramesh kunwar <rameshkunwar369@gmail.com>
2022-03-07 21:14:30 +0545

    Fibonacci series

---
+++ src/com/ramesh/Basics01/Fibonacci07.java
@@ -0,0 +1,10 @@
+package com.ramesh.Basics01;
+
+public class Fibonacci07 {
+    public static void main(String[] args) {
+//        To calculate Fibonacci Series up to n numbers.
+//        0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+        
+    }
+}
