224b37d6345f147bd643ff2a967fc09aa32203fa
ramesh kunwar <rameshkunwar369@gmail.com>
2022-03-08 17:43:17 +0545

    Factorial of a number

--- src/com/ramesh/Basics01/Factorial.java
+++ src/com/ramesh/Basics01/Factorial.java
@@ -1,10 +1,13 @@
 package com.ramesh.Basics01;
 
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
         // Factorial of a give number => 4! == 4*3*2*1
-
-        int n = 12;
+Scanner input = new Scanner(System.in);
+        System.out.printf("Enter a number: ");
+        int n = input.nextInt();
         long factorial = 1;
         for (int i = n; i >=1 ; i--) {
             factorial = factorial *i;
