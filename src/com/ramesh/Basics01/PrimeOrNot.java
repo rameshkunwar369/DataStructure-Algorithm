01999cf3fb7ce53d469c68baaf8e2ee8fa85a725
ramesh kunwar <rameshkunwar369@gmail.com>
2022-03-14 16:11:23 +0545

    some shanges made

---
+++ src/com/ramesh/Basics01/PrimeOrNot.java
@@ -0,0 +1,29 @@
+package com.ramesh.Basics01;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(isPrime(n));
+
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <=1) {
+            return false;
+        }
+        int c = 2;
+        while (c* c <=n){
+            if (n %c ==0) {
+                return  false;
+            }
+            c++;
+        }
+        if (c * c> n){
+            return  true;
+        }
+        return  false;
+    }
+}
