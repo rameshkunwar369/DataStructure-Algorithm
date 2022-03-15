50a6d05ef3c837d0b8311a03afac9e5ca4e2a297
ramesh kunwar <rameshkunwar369@gmail.com>
2022-03-15 16:40:30 +0545

    Largest among given 3 input numbers using function

--- src/com/ramesh/Functions/Largest.java
+++ src/com/ramesh/Functions/Largest.java
@@ -1,8 +1,35 @@
 package com.ramesh.Functions;
 
+import java.util.Scanner;
+
 public class Largest {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        // Program to print largest number among 3 numbers. 
+
+        // Program to print largest number among 3 numbers.
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter num1: ");
+        int num1 = input.nextInt();
+        System.out.printf("Enter num2: ");
+
+        int num2 = input.nextInt();
+        System.out.printf("Enter num3: ");
+
+        int num3 = input.nextInt();
+
+        int large = largest(num1, num2, num3);
+        System.out.println(large + " is largest number among three numbers");
     }
+
+    private static int largest(int n1, int n2, int n3) {
+        int greatest = n1;
+        if (n1 > n2 && n1 > n3) {
+            greatest = n1;
+        } else if (n2 > n1 && n2 > n3) {
+            greatest = n2;
+        } else if (n3 > n2 && n3 > n1) {
+            greatest = n3;
+        }
+        return greatest;
+    }
+
 }
